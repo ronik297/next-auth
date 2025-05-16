0d847b4a87e5d42ce30fed956dc098b4a32a6273
ronik297 <ronik.allen@gmail.com>
2025-05-16 21:15:34 +0530

    checking git commits

--- auth.ts
+++ auth.ts
@@ -3,7 +3,6 @@ import authConfig from "./auth.config"
 import { PrismaAdapter  } from "@auth/prisma-adapter"
 import { db } from "@/lib/db"
 
-
 export const { handlers : { GET, POST }, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" }, 
